6428a717bbc35322b5f382e4335395a25e26b7f1
Sheri Foster <87589967+SFosR@users.noreply.github.com>
2021-12-08 19:28:28 -0500

    final version

--- Assets/script.js
+++ Assets/script.js
@@ -1,70 +1,99 @@
 var apiKey = "4cf6b6df17390c13ea85712d0b2dd185";
 var weatherApiRootURL = "https://api.openweathermap.org";
 var searchHistory = [];
-
-var searchedCities = document.querySelector("#search-history"); //cities
-var searchInput = document.querySelector("#search-input"); //cityInput
-var searchForm = document.querySelector("#search-form"); //formEl
-
+var fiveDayEl = document.querySelector("#five-day-forecast");
+var searchedCities = document.querySelector("#search-history");
+var searchInput = document.querySelector("#search-input");
+var searchForm = document.querySelector("#search-form");
 
 searchForm.addEventListener("submit", function (event) {
   event.preventDefault();
   var cityName = document.getElementById("search-input").value;
+  searchHistory.push(cityName);
+  localStorage.setItem("history", JSON.stringify(searchHistory));
+
   var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
   fetch(queryURL)
     .then((response) => response.json())
     .then((data) => {
       // current weather
-      var currentWeatherContainer = document.getElementById("current-weather");
+      var latitude = data.coord.lat;
+      var longitude = data.coord.lon;
+      var queryFiveDay = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${apiKey}`;
 
-      var cityName = document.getElementById("current-city-name");
-      cityName.innerHTML = data.name;
+      fetch(queryFiveDay)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          var currentWeatherContainer =
+            document.getElementById("current-weather");
 
-      var currentDate = document.getElementById("current-date");
+          var cityNameEl = document.getElementById("current-city-name");
+          cityNameEl.innerHTML = cityName;
 
-      const date = new Date(data.dt * 1000);
-      currentDate.innerHTML = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+          var currentDate = document.getElementById("current-date");
 
-      const currentIcon = document.getElementById("current-icon");
-      currentIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+          const date = new Date(data.current.dt * 1000);
+          currentDate.innerHTML = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
 
-      //        -city name
-      //        -date
-      //        -icon
-      // -temperature
-      // -humidity
-      // -wind speed
-      //  -uv index
-      //      -color for favorable, moderate or high
+          const currentIcon = document.getElementById("current-icon");
+          currentIcon.src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
 
-      console.log(data);
+          var currentTemp = document.getElementById("current-temp");
+          currentTemp.innerHTML =
+            "Temperature: " +
+            Math.floor((data.current.temp - 273.15) * 1.8 + 32);
+
+          var humidity = document.getElementById("current-humidity");
+          humidity.innerHTML = "Humidity: " + data.current.humidity;
+
+          var windSpeed = document.getElementById("current-windspeed");
+          windSpeed.innerHTML = "Wind Speed: " + data.current.wind_speed;
+
+          var uvi = document.getElementById("current-uvi");
+          uvi.innerHTML = "UVI: " + data.current.uvi;
+
+          if (data.current.uvi < 1) {
+            uvi.setAttribute("style", "background-color: green");
+          } else if (data.current.uvi < 3) {
+            uvi.setAttribute("style", "background-color: yellow");
+          } else {
+            uvi.setAttribute("style", "background-color: red");
+          }
+
+          
+          // function that creates the 5 day forecast cards
+          fiveDayEl.innerHTML = "";
+          for (let i = 1; i < 6; i++) {
+            console.log(data.daily[i]);
+            var daily = data.daily[i];
+            var fiveDayCard = document.createElement("div");
+            fiveDayCard.classList.add("card")
+            
+            var dailyTemp = document.createElement("p");
+            dailyTemp.innerHTML ="Temperature: " +
+              Math.floor((daily.temp.day - 273.15) * 1.8 + 32);
+              fiveDayCard.appendChild(dailyTemp);
+            
+            var dailyIcon = document.createElement("img");
+            dailyIcon.setAttribute("src" , `http://openweathermap.org/img/wn/${daily.weather[0].icon}@2x.png`); 
+            fiveDayCard.appendChild(dailyIcon);
+
+            var dailyHumidity = document.createElement("p");
+            dailyHumidity.innerHTML = "Humidity: " + daily.humidity;
+            fiveDayCard.appendChild(dailyHumidity);
+            
+
+            var dailyWindSpeed = document.createElement("p");
+            dailyWindSpeed.innerHTML = "Wind Speed: " + daily.wind_speed;
+            fiveDayCard.appendChild(dailyWindSpeed);
+            fiveDayEl.appendChild(fiveDayCard);
+
+
+          }
+        });
     });
 });
 
-// WHEN I view future weather conditions for that city
-// THEN
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
 
-// 1. search for the city
-
-//     -current conditions
-//        -city name
-//        -date
-//        -icon
-// -temperature
-// -humidity
-// -wind speed
-//  -uv index
-//      -color for favorable, moderate or high
-
-//     -future conditions
-//      -5-day forecast
-//           -date,
-//           -an icon representation of weather conditions,
-//            - the temperature,
-//            -the wind speed,
-//        -the humidity
-//
-//     -add to search history
--- README.md
+++ README.md
@@ -4,15 +4,13 @@
 This app allows clients to see the weather outlook for multiple cities for when they want to make travel plans. Clients can search by city and will see the current conditions, 5-day forecast, temperature, humidity levels, wind speed and UV index. It will also save the clients search history for quick access when clients visit the app again. 
   
 # Technologies Used
-  * JavaScript
-  * HTML
-  * Bootstrap
-  * OpenWeather API
+  JavaScript
+  jQuery
+  HTML
+  CSS
+  OpenWeather API
   
 # Look
-
-<img width="1440" alt="weatherdash pic copy" src="https://user-images.githubusercontent.com/87589967/145113082-c15e1673-88e8-4cf5-b739-f594cd5195a8.png">
-
 
 # Created By:
   Sheri Foster  fostr.sheri@gmail.com 
